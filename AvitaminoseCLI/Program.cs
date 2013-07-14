3af4e8c564b38b12cd9af63da006120848e3fbff
Arnaud Barisain Monrose <abarisain@gmail.com>
2013-07-14 19:38:06 -0400

    Added CLI interface

--- AvitaminoseCLI/Program.cs
+++ AvitaminoseCLI/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Avitaminose;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,20 @@ namespace AvitaminoseCLI
 	{
 		static void Main(string[] args)
 		{
+			System.IO.StreamReader programFile = new System.IO.StreamReader(args[1]);
+			string program = programFile.ReadToEnd();
+			programFile.Close();
+
+			VirtualMachine vm = new VirtualMachine();
+			Assembler assembler = new Assembler(vm.Flow);
+			foreach (string line in program.Split('\n'))
+			{
+				assembler.AddLineToFlow(assembler.AssembleLine(line));
+			}
+			assembler.Finish();
+
+			vm.CPU.Run();
+			Console.WriteLine(((BasicBIOS)vm.BIOS).output);
 		}
 	}
 }
